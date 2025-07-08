e817bafb492ebddc6c96f240ab54719b6573235d
boswashere <136892395+boswashere@users.noreply.github.com>
2025-07-08 23:55:34 +0300

    Create sybau.js

---
+++ pages/sybau.js
@@ -0,0 +1,18 @@
+export default function Sybau() {
+  return (
+    <div style={{
+      height: '100vh',
+      backgroundColor: 'black',
+      color: 'white',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontFamily: 'monospace',
+      fontSize: '1.5rem',
+      textAlign: 'center',
+      padding: '1rem',
+    }}>
+      please kill me, Sarah
+    </div>
+  )
+}
