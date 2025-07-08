141d829e273a1b27afa768eaeec31281efb13b72
boswashere <136892395+boswashere@users.noreply.github.com>
2025-07-09 00:24:44 +0300

    Update sybau.js

--- pages/sybau.js
+++ pages/sybau.js
@@ -8,36 +8,34 @@ export default function x1() {
           color: white;
           font-family: monospace, monospace;
           overflow: hidden;
-        }
-        .container {
-          height: 100vh;
           display: flex;
-          flex-direction: column;
           justify-content: center;
           align-items: center;
+        }
+        .container {
           text-align: center;
           padding: 1rem;
         }
         .text {
           font-size: 2rem;
           user-select: none;
-          white-space: nowrap;
           margin-bottom: 1rem;
+          white-space: nowrap;
         }
         iframe {
           border-radius: 8px;
           box-shadow: 0 0 15px rgba(255, 255, 255, 0.2);
+          width: 300px;
+          height: 166px;
         }
       `}</style>
       <div className="container">
         <div className="text">please kill me, Sarah</div>
         <iframe
-          width="300"
-          height="80"
-          scrolling="no"
+          src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/keepsecrets/you-will-never-change&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           frameBorder="no"
+          scrolling="no"
           allow="autoplay"
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1429412885&color=%23ffffff&auto_play=true&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true"
           title="You Will Never Change - KeepSecrets"
         />
       </div>
