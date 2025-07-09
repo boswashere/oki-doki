e03ac99187b46ddc191066e88853abb4679d78dd
boswashere <136892395+boswashere@users.noreply.github.com>
2025-07-09 04:17:38 +0300

    Update [id].ts

--- pages/api/scripts/[id].ts
+++ pages/api/scripts/[id].ts
@@ -6,17 +6,13 @@ const redis = new Redis({
   token: process.env.KV_REST_API_TOKEN!,
 })
 
-const expectedUA = 'roblox-custom-ua-0987fhsdkj'
-const expectedToken = 'super-secret-token-23423'
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const ua = req.headers['user-agent']
-  const token = req.headers['x-auth-token']
+export default async function someshit(req: NextApiRequest, res: NextApiResponse) {
+  const bsdata = req.headers['x-bsdata']
   const hwid = req.headers['x-hwid']
+  if (!bsdata || !hwid) return res.status(403).send('kms')
 
-  if (!ua || !token || !hwid) return res.status(403).send('kms')
-  if (ua !== expectedUA) return res.status(403).send('kms')
-  if (token !== expectedToken) return res.status(403).send('kms')
+  const expectedBsdata = await redis.get(`token:${hwid}`)
+  if (expectedBsdata !== bsdata) return res.status(403).send('kms')
 
   const id = req.query.id
   if (!id || typeof id !== 'string') return res.status(400).send('kms')
@@ -24,6 +20,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const script = await redis.get(`script:${id}`)
   if (!script) return res.status(404).send('kms')
 
-  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('content-type', 'text/plain')
   res.status(200).send(script)
 }
