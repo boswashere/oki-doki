b55f44d72b132c07767cf5f30dcdbcff16b10c35
boswashere <136892395+boswashere@users.noreply.github.com>
2025-07-09 04:24:31 +0300

    Update get_token.ts

--- pages/api/get_token.ts
+++ pages/api/get_token.ts
@@ -9,12 +9,12 @@ const redis = new Redis({
 
 function someshit() {
   let s = ''
-  const c = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-  for (let i = 0; i < 64; i++) s += c[Math.floor(Math.random() * c.length)]
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  for (let i = 0; i < 64; i++) s += chars[Math.floor(Math.random() * chars.length)]
   return s
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function someshit(req: NextApiRequest, res: NextApiResponse) {
   const bsdata = someshit()
   const hwid = uuidv4()
   await redis.setex(`token:${hwid}`, 15, bsdata)
