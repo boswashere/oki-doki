ab108815a689ca562e04e12ccc0e0c847109eda0
boswashere <136892395+boswashere@users.noreply.github.com>
2025-07-09 03:22:34 +0300

    Create [id].ts

---
+++ pages/api/final_payload/[id].ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Redis } from '@upstash/redis'
+
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL!,
+  token: process.env.KV_REST_API_TOKEN!,
+})
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query
+  if (!id || typeof id !== 'string') return res.status(400).send('kms')
+
+  const ua = req.headers['user-agent'] || ''
+  const isRoblox = typeof ua === 'string' && ua.toLowerCase().includes('roblox')
+  if (!isRoblox) return res.status(403).send('-- kms --')
+
+  const finalScript = await redis.get(`final:${id}`)
+  if (!finalScript) return res.status(404).send('kms')
+
+  res.setHeader('Content-Type', 'text/plain')
+  res.status(200).send(finalScript)
+}
