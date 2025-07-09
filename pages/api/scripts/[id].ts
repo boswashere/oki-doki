4c26f366a87f8b59b7490d9682e16ec32f19f910
boswashere <136892395+boswashere@users.noreply.github.com>
2025-07-09 03:17:14 +0300

    Update [id].ts

--- pages/api/scripts/[id].ts
+++ pages/api/scripts/[id].ts
@@ -9,17 +9,16 @@ const redis = new Redis({
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query
   if (!id || typeof id !== 'string') return res.status(400).send('kms')
-  const script = await redis.get(`script:${id}`)
-  if (!script) return res.status(404).send('i wanna die')
 
-  const ua = req.headers['user-agent'] || ''
-  const allow = typeof ua === 'string' && ua.toLowerCase().includes('roblox')
-  if (!allow) {
-    res.statusCode = 403
-    res.setHeader('Content-Type', 'text/plain')
-    return res.end('-- die die die\n-- kms')
-  }
+  const realid = await redis.get<string>(`loader:${id}`)
+  if (!realid) return res.status(404).send('i wanna die')
+
+  const domain = req.headers.host?.startsWith('localhost')
+    ? 'http://' + req.headers.host
+    : 'https://' + req.headers.host
+
+  const payload = `loadstring(game:HttpGet("${domain}/api/data/${realid}",true))()`
 
   res.setHeader('Content-Type', 'text/plain')
-  res.status(200).send(script)
+  res.status(200).send(payload)
 }
