ffa2f42f27c8d071c510592e74bba1fc71f37e4f
boswashere <136892395+boswashere@users.noreply.github.com>
2025-07-09 03:23:54 +0300

    Update [id].ts

--- pages/api/scripts/[id].ts
+++ pages/api/scripts/[id].ts
@@ -10,14 +10,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { id } = req.query
   if (!id || typeof id !== 'string') return res.status(400).send('kms')
 
-  const realid = await redis.get<string>(`loader:${id}`)
-  if (!realid) return res.status(404).send('i wanna die')
+  const secondId = await redis.get<string>(`loader:${id}`)
+  if (!secondId) return res.status(404).send('i wanna die')
 
   const domain = req.headers.host?.startsWith('localhost')
     ? 'http://' + req.headers.host
     : 'https://' + req.headers.host
 
-  const payload = `loadstring(game:HttpGet("${domain}/api/data/${realid}",true))()`
+  const payload = `loadstring(game:HttpGet("${domain}/api/data/${secondId}",true))()`
 
   res.setHeader('Content-Type', 'text/plain')
   res.status(200).send(payload)
