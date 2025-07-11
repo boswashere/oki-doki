79bcc336030a64bac922e4732dcae1fde53ddac0
boswashere <136892395+boswashere@users.noreply.github.com>
2025-07-11 15:42:44 +0300

    Update [id].ts

--- pages/api/scpt/[id].ts
+++ pages/api/scpt/[id].ts
@@ -1,17 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Redis } from '@upstash/redis'
+import { store } from '../smthg'
 
-const r = new Redis({
-  url: process.env.kv_rest_api_url!,
-  token: process.env.kv_rest_api_token!,
-})
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') return res.status(405).end()
   const { id } = req.query
-  if (!id || typeof id !== 'string') return res.status(400).send('kms')
-  const script = await r.get<string>(`script:${id}`)
-  if (!script) return res.status(404).send('kms')
+  if (typeof id !== 'string') return res.status(400).end('kms')
+  const script = store.get(id)
+  if (!script) return res.status(404).end('kms')
   res.setHeader('content-type', 'text/plain')
   res.status(200).send(script)
 }
