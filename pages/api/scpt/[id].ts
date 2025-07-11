2eb7fee6291023cb05be5c67f78ca6fdec1684bb
boswashere <136892395+boswashere@users.noreply.github.com>
2025-07-11 20:13:18 +0300

    Update [id].ts

--- pages/api/scpt/[id].ts
+++ pages/api/scpt/[id].ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Redis } from '@upstash/redis'
 
-const r = new Redis({
+const redis = new Redis({
   url: process.env.kv_rest_api_url!,
   token: process.env.kv_rest_api_token!,
 })
@@ -9,11 +9,11 @@ const r = new Redis({
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') return res.status(405).end()
   const ua = req.headers['user-agent'] || ''
-  if (!ua.includes('Roblox')) return res.status(403).end()
-  const { id } = req.query
+  if (!ua.toLowerCase().includes('roblox')) return res.status(403).end()
+  const id = req.query.id as string
   if (!id || typeof id !== 'string') return res.status(400).end()
-  const script = await r.get(`script:${id}`)
-  if (!script) return res.status(404).end()
+  const script = await redis.get(`script:${id}`)
+  if (!script || typeof script !== 'string') return res.status(404).end()
   res.setHeader('content-type', 'text/plain')
-  res.status(200).send(script)
+  res.send(script)
 }
